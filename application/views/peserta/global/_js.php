7bffb8dd6f248525fe721970a416fd1a1b5db688
kuroiyuki48 <nooralizahar@gmail.com>
2019-11-07 10:44:05 +0700

    fix datatables peserta

--- application/views/peserta/global/_js.php
+++ application/views/peserta/global/_js.php
@@ -2,5 +2,5 @@
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="<?= base_url('assets/desain/site/docs/4.3/assets/js/vendor/jquery-slim.min.js') ?>"></script>
     <script src="<?= base_url('assets/desain/assets/js/vendor/popper.min.js') ?>"></script>
-    <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.4.js"></script>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="<?= base_url('assets/desain/dist/js/bootstrap.min.js') ?>"></script>
